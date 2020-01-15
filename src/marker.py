37bb38eab51e5189e40e1177eca7b6ad3603cdbf
Yifei Xing <yifeixing@Yifeis-MacBook-Pro-2.local>
2020-01-15 20:37:22 +0800

    S

--- src/marker.py
+++ src/marker.py
@@ -81,8 +81,8 @@ def markup_3dhp():
 
   for i in params:
     img_open[i[0]][i[1]] = [255, 255, 0]
-    for j in [-2,-1, 1,2, 3, 4, 5]:
-      for k in [-2,-1, 1,2, 3, 4, 5]:
+    for j in [-5, -4, -3, -2,-1, 1,2, 3, 4, 5]:
+      for k in [-5, -4, -3, -2,-1, 1,2, 3, 4, 5]:
         img_open[i[0]+j][i[1]+k] = [255, 0, 0]
 
   plt.imshow(img_open)
